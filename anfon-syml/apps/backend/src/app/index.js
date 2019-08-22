1ed9d3cebe142f49c9088e6c379addcf4033bd05
TeasdaleP <pteasdale55@gmail.com>
2019-08-22 22:15:19 +0100

    finalising backend

--- anfon-syml/apps/backend/src/app/index.js
+++ anfon-syml/apps/backend/src/app/index.js
@@ -1,19 +1,160 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const firebase = require('firebase');
+var express = require("express");
+var bodyParser = require("body-parser");
+var firebase = require("firebase");
 
-const app = express();
+var app = express();
 app.use(bodyParser.json());
 
-const config = require('./config/firebase')
-firebase.initializeApp(config);
+var firebaseConfig = {
+  apiKey: "AIzaSyD2wURbsSpJ0GJzkSkNkQgFgyli5osfVv8",
+  authDomain: "anfon-syml.firebaseapp.com",
+  databaseURL: "https://anfon-syml.firebaseio.com",
+  projectId: "anfon-syml",
+  storageBucket: "anfon-syml.appspot.com",
+  messagingSenderId: "956001413127",
+  appId: "1:956001413127:web:b43ce18a82f59f62"
+};
 
-const { users, templates, communications } = require('./routes');
+firebase.initializeApp(firebaseConfig);
 
-app.use('/', users);
-app.use('/templates', templates);
-app.use('/communications', communications);
+// ----------------------------------
+// -- Main
+app.get("/", function(req, res) {
+    console.log("HTTP GET Request");
+    var userReference = firebase.database().ref("/");
+    userReference.on(
+      "value",
+      function(snapshot) {
+        console.log(snapshot.val());
+        res.json(snapshot.val());
+        userReference.off("value");
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        res.send("The read failed: " + errorObject.code);
+      }
+    );
+});
 
-app.listen(process.env.PORT, () =>
-    console.log('Application Listening!'),
-);
+app.put("/", function(req, res) {
+    console.log("HTTP PUT Request");
+    var TemplateReference = randomString(5);
+    var CreateDate = Date.now();
+    var Reference = req.body.username;
+
+    var Request = {
+      id: randomString(10),
+      firstname: req.body.firstname,
+      surname: req.body.surname,
+      username: req.body.username,
+      password: req.body.password,
+      contact: {
+        telephone: req.body.contact.telephone,
+        email: req.body.contact.email
+      },
+      templates: [
+        {
+          id: TemplateReference,
+          title: req.body.templates[0].title,
+          date: CreateDate,
+          channel: req.body.templates[0].channel,
+          content: req.body.templates[0].content
+        }
+      ],
+      communications: [
+        {
+          customer: {
+            reference: req.body.communications[0].customer.reference,
+            email: req.body.communications[0].customer.email,
+            telephone: req.body.communications[0].customer.telephone
+          },
+          date: CreateDate,
+          templates: TemplateReference
+        }
+      ]
+    };
+
+    firebase
+    .database()
+    .ref(Reference)
+    .set(Request);
+});
+
+app.post("/", function(req, res) {
+    console.log("HTTP POST Request");
+
+    var Reference = req.body.id;
+    var Field = req.body.field;
+    var Value = req.body.value;
+
+    firebase
+    .database()
+    .ref(Reference)
+    .child(Field)
+    .update(Value);
+});
+
+app.delete("/", function(req, res) {
+  console.log("HTTP DELETE Requested");
+  res.send("Cannot delete user information");
+});
+
+// ----------------------------------
+// -- Communications
+app.get("/:id/communications", function(req, res) {
+    console.log("HTTP GET Request");
+});
+
+app.put("/:id/communications", function(req, res) {
+    console.log("HTTP PUT Request");
+});
+
+app.post("/:id/communications", function(req, res) {
+    console.log("HTTP POST Request");
+});
+
+app.delete("/:id/communications", function(req, res) {
+    console.log("HTTP DELETE Requested");
+});
+
+// ----------------------------------
+// -- Templates
+app.get("/:id/templates", function(req, res) {
+    console.log("HTTP GET Request");
+});
+
+app.put("/:id/templates", function(req, res) {
+    console.log("HTTP PUT Request");
+});
+
+app.post("/:id/templates", function(req, res) {
+    console.log("HTTP POST Request");
+});
+
+app.delete("/:id/templates", function(req, res) {
+    console.log("HTTP DELETE Requested");
+});
+
+// ----------------------------------
+// -- Server Config
+var server = app.listen(8080, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
+
+function randomString(length) {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".split(
+    ""
+  );
+  if (!length) {
+    length = Math.floor(Math.random() * chars.length);
+  }
+  var str = "";
+  for (var i = 0; i < length; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return str;
+}
+
+
--- anfon-syml/apps/frontend/src/app/enums/http-urls.enum.ts
+++ anfon-syml/apps/frontend/src/app/enums/http-urls.enum.ts
@@ -1,5 +1,5 @@
 export enum HttpUrl {
-  User = "https://8080-dot-7002925-dot-devshell.appspot.com/?authuser=0",
-  Templates = "https://8080-dot-7002925-dot-devshell.appspot.com/template?authuser=0",
-  Communications = "https://8080-dot-7002925-dot-devshell.appspot.com/communications?authuser=0"
+  User = "http://ec2-34-254-24-230.eu-west-1.compute.amazonaws.com:3000/",
+  Templates = "http://ec2-34-254-24-230.eu-west-1.compute.amazonaws.com:3000/templates",
+  Communications = "http://ec2-34-254-24-230.eu-west-1.compute.amazonaws.com:3000/communications"
 }
